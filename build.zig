bc08cb0bd8836c165731e08d2c48e7ea9ddabd90
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-07-05 03:14:54 +0000

    Added basic build program.

---
+++ build.zig
@@ -0,0 +1,13 @@
+pub const zl = @import("./zig_lib/zig_lib.zig");
+
+pub const Node = zl.build.GenericNode(.{});
+
+pub fn buildMain(allocator: *zl.build.Allocator, toplevel: *Node) void {
+    const node: *Node = toplevel.addBuild(
+        allocator,
+        .{ .kind = .exe },
+        "main",
+        "src/main.zig",
+    );
+    node.descr = "Main binary";
+}
